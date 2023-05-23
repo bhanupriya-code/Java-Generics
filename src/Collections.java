d2581bc87076f74b4db7679c5fe42d989c4c58bc
Bhanupriya <90328118+bhanupriya-code@users.noreply.github.com>
2023-05-23 19:03:34 +0530

    Add files via upload

---
+++ src/Collections.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Collections {
+    public static void main(String[] args) {
+        ArrayList<Object> val = new ArrayList<>();
+        val.add("Bhanu");
+        val.add(7);
+        System.out.println(val.toString());
+
+    }
+}
